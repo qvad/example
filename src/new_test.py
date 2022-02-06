71fc369276e693dc1c50e2d35fa1eb37e7eaaab5
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2022-02-06 21:02:19 +0300

    Qa 1236 (#5)
    * New awesome message
    
    * new test added 11

---
+++ src/new_test.py
@@ -0,0 +1,16 @@
+class abc:
+    def __init__(self):
+        self.a = "a"
+
+    def test1:
+        pass
+
+    def test2:
+        print("a")
+
+    def test3:
+        print("absdbf")
+
+    def test4:
+        print("a")
+        print("new messsage)")
