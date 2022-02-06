7423e1eecafb527f4c7c26e9ca0bf22410008c93
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2022-02-06 20:48:30 +0300

    New awesome message

---
+++ src/new_test.py
@@ -0,0 +1,15 @@
+class abc:
+    def __init__(self):
+        self.a = "a"
+
+    def test1:
+        print("a")
+
+    def test2:
+        print("a")
+
+    def test3:
+        print("a")
+
+    def test4:
+        print("a")
